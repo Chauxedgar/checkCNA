c19ef554aa376fb3238b3d0d1625d6e902bf6248
Chauxedgar <echaux22A@udenar.edu.co>
2026-05-18 11:06:59 -0500

    sistema urls.py

--- backend/sistema_reacreditacion/urls.py
+++ backend/sistema_reacreditacion/urls.py
@@ -1,22 +1,11 @@
-"""
-URL configuration for sistema_reacreditacion project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    # Ruta para el panel de administrador que ya usamos
     path('admin/', admin.site.urls),
+    
+    # Aquí conectamos todas las rutas de nuestra aplicación "usuarios"
+    # El prefijo será 'api/', por lo que la ruta final será 'api/login/'
+    path('api/', include('usuarios.urls')),
 ]
